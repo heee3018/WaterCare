6d2428f7abeb29f02bc9f4733d06c1d17b03f413
heee3018 <heee3018@gmail.com>
2021-07-02 09:38:37 +0900

    Modify

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from config  import Address, Mode
 
 data_buffer = [None] * 14
 
-usb_0 = LXC.Setup(port='/dev/ttyUSB0', address=Address, mode=Mode)
-usb_1 = LXC.Setup(port='/dev/ttyUSB1', address=Address, mode=Mode)
-usb_2 = LXC.Setup(port='/dev/ttyUSB2', address=Address, mode=Mode)
-usb_3 = LXC.Setup(port='/dev/ttyUSB3', address=Address, mode=Mode)
-usb_4 = LXC.Setup(port='/dev/ttyUSB4', address=Address, mode=Mode)
-usb_5 = LXC.Setup(port='/dev/ttyUSB5', address=Address, mode=Mode)
-usb_6 = LXC.Setup(port='/dev/ttyUSB6', address=Address, mode=Mode)
+usb_0 = LXC.Setup(port='/dev/ttyUSB0', addresses=Address, mode=Mode)
+usb_1 = LXC.Setup(port='/dev/ttyUSB1', addresses=Address, mode=Mode)
+usb_2 = LXC.Setup(port='/dev/ttyUSB2', addresses=Address, mode=Mode)
+usb_3 = LXC.Setup(port='/dev/ttyUSB3', addresses=Address, mode=Mode)
+usb_4 = LXC.Setup(port='/dev/ttyUSB4', addresses=Address, mode=Mode)
+usb_5 = LXC.Setup(port='/dev/ttyUSB5', addresses=Address, mode=Mode)
+usb_6 = LXC.Setup(port='/dev/ttyUSB6', addresses=Address, mode=Mode)
 
 sleep(5)
 
