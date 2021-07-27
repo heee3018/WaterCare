08bb77231cbf541f1ec628f7b186fd6a07e39e2f
heee3018 <heee3018@gmail.com>
2021-07-27 15:26:02 +0900

    lxc init 수정

--- drivers/lxc.py
+++ drivers/lxc.py
@@ -28,14 +28,6 @@ class Setup:
                 self.state = 'connected'
                 print(f"[LOG] {self.num} - Successfully opened the port")
                 
-            except OSError as e:
-                error_message = str(e)
-                error_port    = error_message[error_message.find('/dev/ttyUSB'):error_message.find(':')]
-                if error_message[:9] == '[Errno 2]':
-                    print(f"[ERROR] {self.num} - {error_port} Could not open port. {find_count+1}/{FIND_COUNT}")
-                
-                self.state = 'disabled'
-                
             except serialutil.SerialException as e:
                 error_message = str(e)
                 error_port    = error_message[error_message.find('/dev/ttyUSB'):error_message.find(':')]
@@ -44,7 +36,8 @@ class Setup:
                 
                 self.state = 'disabled' 
 
-            print(f"[LOG] {self.num} - {self.state}")
+            finally:
+                print(f"[LOG] {self.num} - {self.state}")
             
     def start_search(self):
         thread = Thread(target=self.find_serial_num, daemon=True)
