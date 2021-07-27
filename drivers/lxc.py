578e58c73b0e4b6beeac06e77e5671722809df92
heee3018 <heee3018@gmail.com>
2021-07-27 16:09:15 +0900

    no message

--- drivers/lxc.py
+++ drivers/lxc.py
@@ -32,7 +32,7 @@ class Setup:
                 break
                 
             except serialutil.SerialException as e:
-                print(e[:9])
+                print(f"error >{e}<")
                 print(f"[ERROR] {self.num} - {e[9:]}")
                 self.state = 'disabled' 
                 continue
