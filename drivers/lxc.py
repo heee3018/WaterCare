79afb2e1dced042c4c53fe4d09cf231870a45f69
heee3018 <heee3018@gmail.com>
2021-07-28 09:02:50 +0900

    no message

--- drivers/lxc.py
+++ drivers/lxc.py
@@ -30,21 +30,21 @@ class Setup:
                     self.ser.open()
                 
                 self.state = 'connected'
-                print(f"{'[LOG]':8} {self.num} - Successfully opened the port")
+                print(f"{'[LOG]':>8} {self.num} - Successfully opened the port")
                 break
             
             except serialutil.SerialException as e:
                 if str(e)[:9] == '[Errno 2]':
-                    print(f"{'[ERROR]':8} {self.num} - Could not open port {self.num}")
+                    print(f"{'[ERROR]':>8} {self.num} - Could not open port {self.num}")
                     
                 elif str(e)[:10] == '[Errno 72]':
-                    print(f"{'[ERROR]':8} {self.num} - {str(e)[10:]}")
+                    print(f"{'[ERROR]':>8} {self.num} - {str(e)[10:]}")
                     
                 self.state = 'disabled' 
                 continue
             
             except OSError:
-                print(f"{'[ERROR]':8} {self.num} - Protocol error")
+                print(f"{'[ERROR]':>8} {self.num} - Protocol error")
                 self.state = 'disabled'
                 continue
             
@@ -57,7 +57,7 @@ class Setup:
         find_count = FIND_COUNT
         while find_count > 0 and self.state == 'connected':
             find_count -= 1
-            print(f"{'[LOG]':8} {self.num} - looking for Serial number that matches {self.num}...")
+            print(f"{'[LOG]':>8} {self.num} - looking for Serial number that matches {self.num}...")
             for reversed_num in flip(SERIAL_NUMBER_LIST):
                 select_command = to_select_command(reversed_num)
                 self.ser.write(select_command)
@@ -68,7 +68,7 @@ class Setup:
                     continue
 
                 if response == b'\xE5':
-                    print(f"{'[LOG]':8} {self.num} - {flip(reversed_num)} and {self.num} were successfully matched !")
+                    print(f"{'[LOG]':>8} {self.num} - {flip(reversed_num)} and {self.num} were successfully matched !")
                     self.data[flip(reversed_num)] = {
                         'state'          : 'detected',
                         'select'         :  select_command,
@@ -99,7 +99,7 @@ class Setup:
                 break 
             
             else:
-                print(f"{'[LOG]':8} {self.num} - Cannot find Serial Number for {self.num}")
+                print(f"{'[LOG]':>8} {self.num} - Cannot find Serial Number for {self.num}")
                 self.state = 'not found'
                 pass
                 
@@ -123,7 +123,7 @@ class Setup:
                         read_data = self.ser.read(39)
                         
                         if read_data == b'':
-                            print(f"{'[ERROR]':8} {self.num} - Eempty response")
+                            print(f"{'[ERROR]':>8} {self.num} - Eempty response")
                             self.error_cumulative += 1
                             break
                         
@@ -133,7 +133,7 @@ class Setup:
                             flow_rate    = get_flow_rate(read_format(read_data, 27, 31))
                             total_volume = get_total_volume(read_format(read_data, 21, 25))
                         except:
-                            print(f"{'[ERROR]':8} {self.num} - Eempty response")
+                            print(f"{'[ERROR]':>8} {self.num} - Eempty response")
                             self.error_cumulative += 1
                             break
                         
@@ -158,13 +158,13 @@ class Setup:
                         if USE_DB:
                             self.db.send(f"INSERT INTO {self.db.table} (time, serial_num, flow_rate, total_volume) VALUES ('{time}', '{serial_num}', '{flow_rate}', '{total_volume}')")
                         
-                        print(f"{'[READ]':8} {self.num} - {time} | {serial_num:^12} | {flow_rate:11.6f} ㎥/h | {total_volume:11.6f} ㎥ |")
+                        print(f"{'[READ]':>8} {self.num} - {time} | {serial_num:^12} | {flow_rate:11.6f} ㎥/h | {total_volume:11.6f} ㎥ |")
                                    
                 else:
                     self.error_cumulative += 1
                 
             if self.error_cumulative >= 10:
-                print(f"{'[ERROR]':8} {self.num} - Error accumulates and restarts..")
+                print(f"{'[ERROR]':>8} {self.num} - Error accumulates and restarts..")
                 self.connect_serial()
                 search_thread = self.start_search_thread()
                 search_thread.join()
