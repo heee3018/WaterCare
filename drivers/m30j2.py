36763df2e63c88ba522a8fa3d163945b1c0589c0
heee3018 <heee3018@gmail.com>
2021-07-29 13:01:03 +0900

    no message

--- drivers/m30j2.py
+++ drivers/m30j2.py
@@ -78,17 +78,15 @@ class M30J2(object):
         temperature_10_3 = str(bin(read[2] | 0x100))[3:]
         temperature_2_0  = str(bin(read[3] | 0x100))[3:]
         print(f"S {address_6_0} R A {pressure_13_8[:2]} {pressure_13_8[2:]} A {pressure_7_0} A {temperature_10_3} A {temperature_2_0} N P")
-        print(read[0])
+
         if (read[0] & 0xc0) == 0x00:
             d_pressure    = (unsigned((read[0] & 0x3f) << 8) + read[1])
             d_temperature = (unsigned((read[2]) << 3) + read[3] >> 5)
             self._pressure    = (d_pressure - self._P1) * (self._P_MAX - self._P_MIN) / self._P2 + self._P_MIN
             self._temperature = (d_temperature * 200) / 2047 - 50
 
-            print('asdfas')
-        else:
-            return False
-        
+        return True
+    
     def pressure(self, conversion=UNITS_bar):
         return self._pressure * conversion
 
